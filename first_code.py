e121717c73dab7802779037e44924fb292f2f186
EC2 Default User <ec2-user@ip-172-31-28-80.eu-central-1.compute.internal>
2022-04-03 08:42:43 +0000

    Added logging functionality to function create_dataframe

--- first_code.py
+++ first_code.py
@@ -1,10 +1,21 @@
 import pandas as pd
+import logging
 
+logging.basicConfig(filename = 'example.log', level = logging.INFO)
 
 def create_dataframe():
+    
+    logging.info('Executing Function')
+    logging.info('Creating Data')
     
     df = pd.DataFrame({"col_1":[1,2,3,4,5],
                         "col_2":["A","B","C","D","E"]
     })
     
+    df.info()
+    
+    logging.info('Data Created')
+    logging.info('Exiting')
     return df
+    
+create_dataframe()
